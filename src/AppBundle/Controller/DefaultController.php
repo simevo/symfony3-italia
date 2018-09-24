250f33ec9e37c96741a486481d7ae36db94570b6
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-09-24 22:43:25 +0200

    bootstrap italia example

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,46 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    /**
+     * @Route("/carousel", name="carousel")
+     */
+    public function carousel()
+    {
+        return $this->render('default/carousel.html.twig', [
+        ]);
+    } 
+    /**
+     * @Route("/kitchen-sink", name="kitchen-sink")
+     */
+    public function kitchensink()
+    {
+        return $this->render('default/kitchen-sink.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/cookiebar", name="cookiebar")
+     */    
+    public function cookiebar()
+    {
+        return $this->render('default/cookiebar.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/offcanvas", name="offcanvas")
+     */    
+    public function offcanvas()
+    {
+        return $this->render('default/offcanvas.html.twig', [
+        ]);
+    }
+    /**
+     * @Route("/comuni", name="comuni")
+     */       
+    public function comuni()
+    {
+        return $this->render('default/comuni.html.twig', [
+        ]);
+    }        
 }
+
